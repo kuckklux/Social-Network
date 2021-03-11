385170f1e0fc8df940cf98645bd64917fb108cff
unknown <kuckklux@gmail.com>
2021-03-12 01:42:34 +0300

    Unti 13 React JS (git-репозиторий)

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+   return <header className='header'>
+   <img src='https://static.tildacdn.com/tild6466-3861-4564-b161-613063636365/56cabad6ea07c15307e9d7af.png'/>
+ </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+   return <div className='content'>
+   <img src='https://sun9-75.userapi.com/impf/-9gwyv4zDTOmtEBx3Ky4QGGvJQXTXyLlmfdLVg/y0-S1T2WxEw.jpg?size=992x246&quality=96&sign=5e86086959fb16470312cc8784bd70cc&type=album'/>
+
+  <div>
+      ava + description
+  </div>
+
+    <div>
+      My posts
+
+      <div>
+        New post
+      </div>
+
+      <div>
+        post 1
+      </div>
+
+      <div>
+        post 2
+      </div>
+      
+  </div>
+</div>
+}
+
+export default Profile;
