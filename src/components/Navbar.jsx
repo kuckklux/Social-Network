da80d78abc0c6bbc841f5710640b3c1785c2fba2
unknown <kuckklux@gmail.com>
2021-03-12 14:00:07 +0300

    Unit 14 css-модули, css-modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
+import s from './Navbar.module.css';
+
+/*let c1 = 'item';
+let c2 = 'active';
+
+let classes = c1 + " " + c2;
+let classesNew = '${s.item} ${s.active}';
+
+let classes = {
+   'nav': 'Navbar_nav__J6b4i ',
+   'item': 'Navbar_item__1qPqD'
+}*/
 
 const Navbar = () => {
-   return <nav className='nav'>
+   return <nav className={s.nav}>
+      <div className ={s.item}>
+          <a>Profile</a>
+      </div>
 
-   <div>
-     <a>Profile</a>
-   </div>
 
-   <div>
+
+   <div className ={`${s.item} ${s.active}`}>
       <a>Messages</a>
    </div>
 
-   <div>
+   <div className ={s.item}>
       <a>News</a>
    </div>
 
-   <div>
+   <div className ={s.item}>
       <a>Music</a>
    </div>
 
-   <div>
+   <div className ={s.item}>
       <a>Settings</a>
    </div>
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
-   return <div className='content'>
+   return <div className={s.content}>
    <img src='https://sun9-75.userapi.com/impf/-9gwyv4zDTOmtEBx3Ky4QGGvJQXTXyLlmfdLVg/y0-S1T2WxEw.jpg?size=992x246&quality=96&sign=5e86086959fb16470312cc8784bd70cc&type=album'/>
 
   <div>
@@ -11,18 +12,19 @@ const Profile = () => {
     <div>
       My posts
 
-      <div>
-        New post
-      </div>
+        <div>
+          New post
+        </div>
 
-      <div>
-        post 1
-      </div>
+        <div className={s.posts}>
+          <div className={s.item}>
+            post 1
+          </div>
 
-      <div>
-        post 2
-      </div>
-      
+          <div className={s.item}>
+            post 2
+          </div>
+        </div>
   </div>
 </div>
 }
