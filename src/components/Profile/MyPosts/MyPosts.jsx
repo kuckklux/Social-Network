8097fb70ff7edec69e104352a66517d711c20135
Forgotten Broth <kuckklux@gmail.com>
2021-03-13 00:04:42 +0300

    Unit 16 Props Homework

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,12 +10,15 @@ const MyPosts = () => {
         <button>Add post</button>
         <button>Remove</button>
         <div className={s.posts}>
-              <Post/>
-              <Post/>
-              <Post/>
-              <Post/>
-              <Post/>
-              <Post/>
+              <Post message = 'Поздравляю, ТЫ ТОЛЬКО ЧТО ГРАНАТУ.' likecounts = '10'/>
+              <Post message = 'Только что что?' likecounts = '15'/>
+              <Post message = 'Гранату, ясное дело.'/>
+              <Post message = 'Что гранату?'/>
+              <Post message = 'Только что.'/>
+              <Post message = 'Что только что?'/>
+              <Post message = 'Гранату.'/>
+              <Post message = 'Что гранату?'/>
+              <Post message = 'Только что.'/>
         </div>
     </div>
 }
