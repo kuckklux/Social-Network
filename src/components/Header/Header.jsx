9f2d8c51ac8b7416b3d8e03f3fa86d6d1f8ba84f
Forgotten Broth <kuckklux@gmail.com>
2021-03-13 03:42:52 +0300

    Unit 18 страница Dialogs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import s from './Header.module.css';
 
 const Header = () => {
-   return <header className={s.header}>
-      <img src='https://static.tildacdn.com/tild6466-3861-4564-b161-613063636365/56cabad6ea07c15307e9d7af.png'/>
- </header>
+    return <header className={s.header}>
+        <img src='https://static.tildacdn.com/tild6466-3861-4564-b161-613063636365/56cabad6ea07c15307e9d7af.png'/>
+    </header>
 }
 
 export default Header;
