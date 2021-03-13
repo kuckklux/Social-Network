a7902b7e2d845cc864218b46d15c585429f46ce4
Forgotten Broth <kuckklux@gmail.com>
2021-03-13 18:14:59 +0300

    Unit 19 route, browser-router + homework

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import s from './Navbar.module.css';
+import React from "react";
+import s from "./Navbar.module.css";
 
 const Navbar = () => {
-   return <nav className={s.nav}>
-      <div className ={s.item}>
-          <a>Profile</a>
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <a href="/profile">Profile</a>
       </div>
 
+      <div className={`${s.item} ${s.active}`}>
+        <a href="/dialogs">Messages</a>
+      </div>
 
+      <div className={s.item}>
+        <a href="/news">News</a>
+      </div>
 
-   <div className ={`${s.item} ${s.active}`}>
-      <a>Messages</a>
-   </div>
+      <div className={s.item}>
+        <a href="/music">Music</a>
+      </div>
 
-   <div className ={s.item}>
-      <a>News</a>
-   </div>
-
-   <div className ={s.item}>
-      <a>Music</a>
-   </div>
-
-   <div className ={s.item}>
-      <a>Settings</a>
-   </div>
-
- </nav>
-}
+      <div className={s.item}>
+        <a href="/settings">Settings</a>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
 
