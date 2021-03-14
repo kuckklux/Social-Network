4e21b15ee0578c05d7a431fea0dbc9c068e5d9a3
Forgotten Broth <kuckklux@gmail.com>
2021-03-14 19:51:45 +0300

    Unit 21 style-update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css';
 
 const Header = () => {
     return <header className={s.header}>
-        <img src='https://static.tildacdn.com/tild6466-3861-4564-b161-613063636365/56cabad6ea07c15307e9d7af.png'/>
+        <img className = {s.logo} src='https://static.tildacdn.com/tild6466-3861-4564-b161-613063636365/56cabad6ea07c15307e9d7af.png'/>
     </header>
 }
 
