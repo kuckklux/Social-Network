0e5ebc842ded0271ebd9c65fd029f66612dde4b1
cth <kuckklux@gmail.com>
2021-03-21 15:46:50 +0300

    Unit 27 прокидываем props через Route

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   return (
@@ -16,11 +16,11 @@ const App = () => {
         <Header />
         <Navbar />
         <div class="app-wrapper-content">
-          <Route path = '/profile' component = {Profile}/>
-          <Route path ='/dialogs' component = {Dialogs}/>
-          <Route path ='/news' component = {News}/>
-          <Route path ='/music' component = {Music}/>
-          <Route path ='/settings' component = {Settings}/>
+          <Route path="/profile" render={() => <Profile />} />
+          <Route path="/dialogs" render={() => <Dialogs />} />
+          <Route path="/news" render={() => <News />} />
+          <Route path="/music" render={() => <Music />} />
+          <Route path="/settings" render={() => <Settings />} />
         </div>
       </div>
     </BrowserRouter>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import s from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
-const Profile = () => {
+const Profile = (props) => {
   return (
     <div>
       <ProfileInfo />
